23d3292c35cf833ba995a9b71bb3bacf0f15a873
XPaladiumyX <allegra.yann@gmail.com>
2025-11-17 22:40:17 +0100

    first commit

---
+++ src/main/java/skyxnetwork/skyXDebugger/SkyXDebugger.java
@@ -0,0 +1,17 @@
+package skyxnetwork.skyXDebugger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyXDebugger extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
